3f792c77d7d032e9b21b08a098556ac8846ea40d
did10 <mathias1999@gmx.net>
2025-06-26 00:23:56 +0200

    Add advanced project to have a test basis for multiple objectives at once

---
+++ LTP_advanced_demo_project/objectives/DontStarve.gd
@@ -0,0 +1,16 @@
+class_name DontStarve2 extends Objective
+
+
+func _init(a: Agent) -> void:
+	super(a, "Dont starve: food > 10")
+	
+func _work():
+	add_task(GatherFood2.new(agent), "gather")
+
+func _can_work():
+	# has no active task or task is resolved
+	return not has_task("gather")
+	
+func _check_resolved():
+	#high number of food         
+	return  agent.food > 10
---
+++ LTP_advanced_demo_project/objectives/DontThirst.gd
@@ -0,0 +1,16 @@
+class_name DontThirst2 extends Objective
+
+
+func _init(a: Agent) -> void:
+	super(a, "Dont thirst: water > 10")
+	
+func _work():
+	add_task(DrinkWater2.new(agent), "gather")
+
+func _can_work():
+	# has no active task or task is resolved
+	return not has_task("gather")
+	
+func _check_resolved():
+	#high number of food         
+	return  agent.water > 10
