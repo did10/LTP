2d5f76fbc1b9f1a31f4d05e9249be0d368316df7
did10 <mathias1999@gmx.net>
2025-06-28 19:51:23 +0200

    Show ticks per second

--- addons/LTP/globals/LTPTicker.gd
+++ addons/LTP/globals/LTPTicker.gd
@@ -4,6 +4,11 @@ var timer: float = 0.0
 var interval: float = 0.3
 var active = false
 var n_tick := 0
+
+var time_since_reset = 0
+var ticks_at_last_reset = 0
+var ticks_per_sec = 0
+
 signal tick(n: int)
 
 func start():
@@ -18,3 +23,9 @@ func _process(delta: float) -> void:
 			timer -= interval
 			tick.emit()
 			n_tick = n_tick + 1
+	time_since_reset += delta
+	if time_since_reset >= 1:
+		ticks_per_sec = (n_tick - ticks_at_last_reset)
+		ticks_at_last_reset = n_tick
+		time_since_reset = 0
+		
